ed9afe04d34286edbf980aa8f60665f971332592
白哲銘 <b09902076@ntu.edu.tw>
2022-12-09 21:49:08 +0800

    1.rand seed is 123  2.n is in the range of 1~1000

---
+++ generator.py
@@ -0,0 +1,50 @@
+import random
+random.seed(123)
+
+path = 'testcase.txt'
+f = open(path, "w")
+
+n = random.randint(1, 1000)
+
+f.write(str(n)) 
+f.write("\r\n")
+
+entr = [0, 5, 6, 11]
+exit = [1, 2, 9, 10]
+zone = [3, 4, 7, 8]
+
+to_pay = [0,0,0,1]
+
+position = [] #to record what zone the car is in at this "arrival time"
+
+arrival_time = 0
+
+for i in range(n):
+    vehicle_id = i
+    
+    start = random.choice(entr)
+    
+    while start in position:   #Avoid generating the car at the same position at the same time
+        start = random.choice(entr)
+
+    position.append(start)
+    
+    end = random.choice(exit)
+    while (start == 0 and end == 1) or (start == 6 and end == 2) or (start == 11 and end == 10) or (start == 5 and end == 9):
+        end = random.choice(exit)
+    
+    is_want_to_pay = random.choice(to_pay) #let most people not want to pay
+
+    if is_want_to_pay:
+        payment = random.randint(1, 10000)
+    else:
+        payment = 0
+
+    f.write(f"{vehicle_id} {arrival_time} {start} {end} {payment}")
+    f.write("\r\n")
+
+    to_add_arrival_time = random.randint(0, 10)     #select the arrival time of next car
+
+    if to_add_arrival_time:
+        arrival_time = arrival_time + to_add_arrival_time
+        position.clear()
